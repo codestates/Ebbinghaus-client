43ea82410d6e43debbf4cd5278db67b0d8291153
Ryujaekang <fbworkd4943@gmail.com>
2020-10-18 21:56:54 +0900

    wordList 구현완료
    서버에 get 요청을 통해 데이터를 받은 후
    받은 word 데이터를 List로 띄움.
    그리고 word 선택 후 test 등록시 test 등록이 가능하도록 구현

--- ebbing/MineWords/MineWords.js
+++ ebbing/MineWords/MineWords.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import {
-  Button,
   FlatList,
   StyleSheet,
+  ActivityIndicator,
   Text,
   View,
   TouchableOpacity,
@@ -11,16 +11,127 @@ import {
 export default class MineWords extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      loading: false,
+      dataSource: [],
+      selectData: [],
+    };
   }
 
-  onClickListener = (viewId) => {
-    Alert.alert('Alert', 'Button pressed ' + viewId);
+  // 랜더링시 데이터를 받아옴
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  // Data를 받아오기 위해 서버에 요청하는 곳
+  //데이터를 받아올 때 상태값으로 isSelect과 selectedClass 를 넣어줌
+  //isSelect 은 item의 선택여부, selectedClass는 그에 따른 스타일 변경
+  fetchData = () => {
+    this.setState({ loading: true });
+
+    let options = {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+      credentials: 'include',
+    };
+
+    fetch('http://localhost:4000/word/mine', options)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        responseJson = responseJson.map((item) => {
+          item.isSelect = false;
+          item.selectedClass = styles.list;
+          return item;
+        });
+        this.setState({
+          loading: false,
+          dataSource: responseJson,
+        });
+      })
+      .catch((error) => {
+        this.setState({ loading: false });
+      });
   };
 
+  //List의 사이사이 빈 공간
+  FlatListItemSeparator = () => <View style={styles.line} />;
+
+  //item의 선택에 대한 함수
+  //선택에 따른 스타일 변경 및
+  selectItem = (data) => {
+    data.item.isSelect = !data.item.isSelect;
+    data.item.selectedClass = data.item.isSelect
+      ? styles.selected
+      : styles.list;
+
+    const index = this.state.dataSource.findIndex(
+      (item) => data.item.id === item.id
+    );
+
+    this.state.dataSource[index] = data.item;
+
+    this.setState({
+      dataSource: this.state.dataSource,
+    });
+  };
+
+  goToTest = (data) => {
+    let result = [];
+    console.log('data1', data);
+    data.forEach((element) => {
+      if (element.isSelect === true) {
+        result.push(element);
+      }
+    });
+    console.log('data2', data);
+
+    let options = {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+      credentials: 'include',
+      body: JSON.stringify({
+        array: [...result],
+      }),
+    };
+    fetch('http://localhost:4000/word/mine/test-register', options).then(
+      this.fetchData()
+    );
+  };
+
+  renderItem = (data) => (
+    <TouchableOpacity
+      style={[styles.list, data.item.selectedClass]}
+      onPress={() => this.selectItem(data)}
+    >
+      <Text style={styles.lightText}>
+        {data.item.word_eng} {data.item.word_kor}
+      </Text>
+    </TouchableOpacity>
+  );
+
   render() {
+    const itemNumber = this.state.dataSource.filter((item) => item.isSelect)
+      .length;
+    if (this.state.loading) {
+      return (
+        <View style={styles.loader}>
+          <ActivityIndicator size="large" color="purple" />
+        </View>
+      );
+    }
+
     return (
       <View style={styles.container}>
-        <View style={styles.title}>
+        <View style={styles.header}>
           <Text style={styles.white}>우선영어단어장</Text>
           <TouchableOpacity style={styles.box}>
             <Text>등록 단어</Text>
@@ -28,31 +139,13 @@ export default class MineWords extends Component {
         </View>
         <FlatList
           style={styles.Words}
-          data={[
-            { key: 'Devin' },
-            { key: 'Dan' },
-            { key: 'Dominic' },
-            { key: 'Jackson' },
-            { key: 'James' },
-            { key: 'Joel' },
-            { key: 'John' },
-            { key: 'Jillian' },
-            { key: 'Jimmy' },
-            { key: 'Julie' },
-            { key: 'Devin1' },
-            { key: 'Dan1' },
-            { key: 'Dominic1' },
-            { key: 'Jackson1' },
-            { key: 'James1' },
-            { key: 'Joel1' },
-            { key: 'John1' },
-            { key: 'Jillian1' },
-            { key: 'Jimmy1' },
-            { key: 'Julie1' },
-          ]}
-          renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+          data={this.state.dataSource}
+          ItemSeparatorComponent={this.FlatListItemSeparator}
+          renderItem={(item) => this.renderItem(item)}
+          keyExtractor={(item) => item.id}
+          extraData={this.state}
         />
-        <View style={styles.title}>
+        <View style={styles.between}>
           <View>
             <TouchableOpacity
               style={styles.box}
@@ -62,8 +155,11 @@ export default class MineWords extends Component {
             </TouchableOpacity>
           </View>
           <View>
-            <TouchableOpacity style={styles.box}>
-              <Text>Test 등록</Text>
+            <TouchableOpacity
+              style={styles.box}
+              onPress={() => this.goToTest(this.state.dataSource)}
+            >
+              <Text>{itemNumber}개 Test 등록</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -90,7 +186,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffff',
     width: 300,
   },
-  title: {
+  header: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: 280,
@@ -100,7 +196,7 @@ const styles = StyleSheet.create({
     color: '#fff',
   },
   box: {
-    width: 80,
+    width: 90,
     height: 30,
     backgroundColor: '#fff',
     alignItems: 'center',
@@ -109,5 +205,44 @@ const styles = StyleSheet.create({
   right: {
     marginTop: 10,
     marginLeft: 200,
+  },
+  title: {
+    fontSize: 20,
+    color: '#fff',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  loader: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  list: {
+    paddingVertical: 5,
+    margin: 2,
+    flexDirection: 'row',
+    backgroundColor: '#192338',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    zIndex: -1,
+  },
+  lightText: {
+    color: '#f7f7f7',
+    width: 300,
+    paddingLeft: 15,
+    fontSize: 20,
+  },
+  line: {
+    height: 0.5,
+    width: '100%',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  selected: { backgroundColor: '#FA7B5F' },
+  between: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 280,
+    paddingBottom: 7,
   },
 });
