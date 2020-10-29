974920c91d4bbfba7e9714962fa6fd1b0caf20bd
Ryujaekang <fbworkd4943@gmail.com>
2020-10-29 18:43:03 +0900

    cssMain 작업 완료

--- ebbing/Main/Main.js
+++ ebbing/Main/Main.js
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
   ScrollView,
   Image,
+  TouchableHighlight,
 } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -21,18 +22,26 @@ export default class Main extends React.Component {
     const { navigation } = this.props;
     return (
       <View style={styles.container}>
+        <Image
+          style={{ width: width, height: 200, opacity: 0.8 }}
+          source={require('../assets/ship.jpg')}
+        />
         <TouchableOpacity onPress={() => navigation.navigate('PriorityWords')}>
           <View style={styles.categoryView}>
             <Image
-              style={{ width: 50, height: 50 }}
+              style={{ width: 80, height: 80 }}
               source={require('../assets/ellipse1.png')}
             />
-            <Text style={styles.categoryText}>우선순위 영단어</Text>
+            <Text style={styles.categoryText}>우선 순위 영단어</Text>
           </View>
         </TouchableOpacity>
 
         <TouchableOpacity onPress={() => navigation.navigate('MineWords')}>
           <View style={styles.categoryView}>
+            <Image
+              style={{ width: 80, height: 80 }}
+              source={require('../assets/ellipse2.png')}
+            />
             <Text style={styles.categoryText}>나만의 영단어</Text>
           </View>
         </TouchableOpacity>
@@ -46,20 +55,24 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     backgroundColor: '#252B39',
     position: 'relative',
   },
   categoryView: {
     marginVertical: 20,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#7ABCD3',
     width: width,
     height: 180,
+    justifyContent: 'space-evenly',
     alignItems: 'center',
-    justifyContent: 'center',
     flexDirection: 'row',
+    borderWidth: 0.5,
+    borderColor: "#6190E8",
+    borderRadius: 15,
   },
   categoryText: {
     fontSize: 20,
+    color: '#fff',
   },
 });
